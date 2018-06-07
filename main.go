dce659963dfd4fb20f2884ae7b81836dce244bd7
zfq <zhangfengqi@elextec.com>
2018-06-07 18:10:18 +0800

    update

--- main.go
+++ main.go
@@ -7,17 +7,35 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"path/filepath"
-	"strings"
+	"github.com/segmentio/ksuid"
+	"time"
 )
 
 const (
 	resource = "resource"
 )
 
+const (
+	FieldId = "_id"
+)
+
+var manager = NewManager()
+
+func newUser(domain string) *http.Cookie {
+	id := ksuid.New().String()
+	cookie := new(http.Cookie)
+	cookie.Path = "/"
+	cookie.Domain = domain
+	cookie.HttpOnly = true
+	cookie.Expires = time.Now().Add(time.Hour * 2)
+	cookie.Value = id
+	cookie.Name = FieldId
+	return cookie
+}
+
 func defaultHandle(w http.ResponseWriter, r *http.Request) {
-	log.Println("request from:", r.RemoteAddr,r.URL.String())
+	log.Println("request from:", r.RemoteAddr, r.URL.String())
 	tpl, err := template.ParseFiles(filepath.Join(resource, "head.html"),
 		filepath.Join(resource, "body.html"),
 		filepath.Join(resource, "index.html"))
@@ -25,22 +43,21 @@ func defaultHandle(w http.ResponseWriter, r *http.Request) {
 		errHandle(w, r, err)
 		return
 	}
-	infos := []os.FileInfo{}
-	walkfunc := func(path string, info os.FileInfo, err error) error {
-		if err != nil || info == nil {
-			return err
-		}
-		if strings.Contains(path, ".git") {
-			return nil
-		}
-		infos = append(infos, info)
-		return nil
-	}
-	filepath.Walk("./", walkfunc)
-	w.Header().Add("Pragma","no-cache")
-	tpl.ExecuteTemplate(w, "index.html", infos)
 
-	//fmt.Fprintln(w, "hello world")
+	c, err := r.Cookie("_id")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			c = newUser(r.Host)
+			http.SetCookie(w, c)
+		} else {
+			errHandle(w, r, err)
+			return
+		}
+	}
+	manager.UserConnect(c.Value)
+	w.Header().Add("Pragma", "no-cache")
+	total := manager.UserCount()
+	tpl.ExecuteTemplate(w, "index.html", total)
 }
 
 func faviconHandle(w http.ResponseWriter, r *http.Request) {
@@ -69,15 +86,15 @@ func serveWs(ws *websocket.Conn) {
 	for {
 		i++
 		var msg string
-		err := websocket.Message.Receive(ws,&msg)
+		err := websocket.Message.Receive(ws, &msg)
 		if err != nil {
-			log.Println(ws.Request().RemoteAddr,err)
+			log.Println(ws.Request().RemoteAddr, err)
 			break
 		}
-		log.Println("from:",ws.Request().RemoteAddr,":",msg)
-		err = websocket.Message.Send(ws,fmt.Sprintf("redten %d",i))
+		log.Println("from:", ws.Request().RemoteAddr, ":", msg)
+		err = websocket.Message.Send(ws, fmt.Sprintf("redten %d", i))
 		if err != nil {
-			log.Println(ws.Request().RemoteAddr,err)
+			log.Println(ws.Request().RemoteAddr, err)
 			break
 		}
 	}
@@ -99,7 +116,7 @@ func main() {
 	}
 }
 
-func listenWebsocket(){
+func listenWebsocket() {
 	http.Handle("/ws", websocket.Handler(websocketHandle))
 	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
---
+++ session.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type SessionManager struct{
+	users map[string]interface{}
+	lock sync.Mutex
+}
+
+func NewManager()*SessionManager{
+	m := new(SessionManager)
+	m.users = make(map[string]interface{},0)
+	return m
+}
+
+func (sm *SessionManager)UserConnect(uuid string){
+	sm.lock.Lock()
+	defer sm.lock.Unlock()
+	_,ok := sm.users[uuid]
+	if ok {
+		return
+	}
+	sm.users[uuid] = time.Now()
+}
+
+func (sm *SessionManager)UserCount()int{
+return len(sm.users)
+}
+
