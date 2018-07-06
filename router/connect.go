6b24905775a2e4130b10a753874fb7068991fac1
zfq <zhangfengqi@elextec.com>
2018-07-06 18:07:21 +0800

    l

--- router/connect.go
+++ router/connect.go
@@ -4,22 +4,45 @@ import (
 	"golang.org/x/net/websocket"
 	"github.com/segmentio/ksuid"
 	"log"
+	"sync"
 )
 
 type Connect struct {
-	ws      *websocket.Conn
-	id      string
-	chin chan []byte	//channel to receive ws msg
-	chout chan []byte   //channel to send ws msg
+	ws    *websocket.Conn
+	id    string
+	chin  chan []byte //channel to receive ws msg
+	chout chan []byte //channel to send ws msg
+}
+
+var (
+	connMap map[string]*Connect
+	clock sync.Mutex
+)
+
+func ServeConnect(c *Connect){
+	clock.Lock()
+	connMap[c.ID()] = c
+	clock.Unlock()
+	c.active()
 }
 
 func NewConnect(ws *websocket.Conn) *Connect {
 	return &Connect{
-		ws:      ws,
-		id:      ksuid.New().String(),
-		chin: make(chan []byte, 0),
-		chout: make(chan []byte,0),
+		ws:    ws,
+		id:    ksuid.New().String(),
+		chin:  make(chan []byte, 0),
+		chout: make(chan []byte, 0),
 	}
+}
+
+func (c *Connect) ID() string {
+	return c.id
+}
+
+func (c *Connect) Close(){
+	c.ws.Close()
+	close(c.chin)
+	close(c.chout) // fixme
 }
 
 func (c *Connect) active() {
@@ -38,7 +61,7 @@ func (c *Connect) wsRecv() {
 		select {
 		case c.chin <- buf:
 		default:
-			log.Println(c.id,"ws chan in blocked")
+			log.Println(c.id, "ws chan in blocked")
 		}
 	}
 	log.Println(c.id, "ws recv quit")
@@ -47,14 +70,14 @@ func (c *Connect) wsRecv() {
 func (c *Connect) wsSend() {
 	for {
 		select {
-		case buf,ok := <-c.chout:
+		case buf, ok := <-c.chout:
 			if !ok {
-				log.Println(c.id,"ws closed")
+				log.Println(c.id, "ws closed")
 				break
 			}
-			err := websocket.Message.Send(c.ws,buf)
+			err := websocket.Message.Send(c.ws, buf)
 			if err != nil {
-				log.Println(c.ws.RemoteAddr(),err)
+				log.Println(c.ws.RemoteAddr(), err)
 				break
 			}
 		}
